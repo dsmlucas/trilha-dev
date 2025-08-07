464f922af6cda7cdc6ad5b3bdbcf2535cf8fcb43
Gustavo dos Santos <gustavodosantosmadeira@gmail.com>
2025-08-07 10:03:52 -0300

    Adicionando conteúdos da aula 3: Tomada de Decisões

---
+++ etapa-1/capitulo-1/aula-3/aula28.py
@@ -0,0 +1,20 @@
+#Tomada de Decisões 2
+
+num1= int(input("Digite um número: "))
+
+if(num1 >15) :
+    print("O valor é maior que 15!")
+    if(num1<=30) :
+         print("O valor é maior do que 10 porem menor que 30.")
+    else:
+        if(num1>50) :
+            print("O valor é maior que 30, porem menor ou igual a  50.")
+else:
+    if(num1>5) :
+        print ("O número é menor que 15, porem maior que 5.")
+        if(num1==8) :
+            print ("O número é igual a 8.")
+
+    else: 
+        print ("O valor é menor que 5.") 
+
